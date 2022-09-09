7d34c935e79d50df194d60529a0b93fdb8e03c24
Md AL Mamun Mim <58371734+Mamun1646@users.noreply.github.com>
2022-09-09 16:23:02 +0600

    Update Product.js

--- src/component/Product.js
+++ src/component/Product.js
@@ -8,7 +8,7 @@ const Product = (props) => {
       <div className="product__details">
         <h4 className="product__title">{product.title}</h4>
         <p className="product__price">Price: $ {product.price}</p>
-        <p product__rating>Rating: {product.rating.rate}/5</p>
+        <p className="product__rating">Rating: {product.rating.rate}/5</p>
         <p className="product__desc">Description: {product.description}</p>
         <button className="product__btn btn">Add to cart</button>
       </div>
