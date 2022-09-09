b7424bb91fd4872276732e0543acebddae8b8718
mamun1646 <mimbd01130004@gmail.com>
2022-09-09 16:16:00 +0600

    online clothing store designing using REACT

---
+++ src/component/Products.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Product from "./Product";
+
+const Products=(props)=>{
+  const {products} =props;
+  return (
+    <section className="products">
+      {products.map((product)=>
+        <Product key ={product.id} product ={product}/>
+    )}
+    </section>
+
+  )
+}
+export default Products;
